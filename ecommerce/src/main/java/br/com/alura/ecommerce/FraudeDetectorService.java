1073da751d52ca2f6a9b0949c404cca6e5911de1
Jonathan Philipe <nt.jonathan@agi.com.br>
2024-04-30 17:06:28 -0300

    Serialização customizada

--- ecommerce/src/main/java/br/com/alura/ecommerce/FraudeDetectorService.java
+++ ecommerce/src/main/java/br/com/alura/ecommerce/FraudeDetectorService.java
@@ -2,16 +2,22 @@ package br.com.alura.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.util.Map;
+
 public class FraudeDetectorService {
 
     public static void main(String[] args) {
         var fraudeService = new FraudeDetectorService();
-        try(var service = new KafkaService(FraudeDetectorService.class.getSimpleName(), "ECOMMERCE_NEW_ORDER", fraudeService::parse)) {
+        try(var service = new KafkaService<>(FraudeDetectorService.class.getSimpleName(),
+                "ECOMMERCE_NEW_ORDER",
+                fraudeService::parse,
+                Order.class,
+                Map.of())) {
             service.run();
         }
     }
 
-    private void parse(ConsumerRecord<String, String> record) {
+    private void parse(ConsumerRecord<String, Order> record) {
         System.out.println("----------------------------------------");
         System.out.println("Processing new order, checking for fraud");
         System.out.println(record.key());
